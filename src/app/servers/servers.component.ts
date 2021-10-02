cff5609b3d9af39ece2c94d1dd11b3b2e6ad8466
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-02 12:58:06 -0400

    adding different selectors for ServersComponent

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 // template has to be present in the Component
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+
+  // selecting the element by attribute
+  // selector: '[app-servers]',
+
+  // selecting the element by class
+  selector: '.app-servers',
+
   // either has to be a link to an external or inner template
   // templateUrl: './servers.component.html',
   // template: - (allows us to define html code in the same file) - inline template
