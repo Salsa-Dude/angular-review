16e8200009812cfcd9522fc25f29b8e7ad2cc56f
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-02 12:36:27 -0400

    implemented css styling for AppComponent using inline and external styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  // inline style
+  styles: [`
+    h3 {
+      color: yellow;
+    }
+  `]
 })
 export class AppComponent {
   name = 'joseph';
